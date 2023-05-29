918dc53233a27c4301dccda283aaaecc59a654a5
SidneiHJr <sidneihjr@gmail.com>
2023-05-29 12:55:41 -0300

    solution structure and swagger configurations

---
+++ src/TaskManager.API/Configuration/SwaggerConfig.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace TaskManager.API.Configuration
+{
+    public static class SwaggerConfig
+    {
+        public static IServiceCollection AddSwaggerConfig(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Title = "Task Manager Api",
+                    Description = "Api for task management",
+                    Contact = new OpenApiContact { Name = "Sidnei", Email = "sidneihjr@gmail.com", Url = new Uri("https://www.linkedin.com/in/sidnei-hernandes-junior-217621209/") }
+                });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                    {
+                        new OpenApiSecurityScheme {
+                            Reference = new OpenApiReference {
+                                    Type = ReferenceType.SecurityScheme,
+                                        Id = "Bearer"
+                                },
+                                Scheme = "oauth2",
+                                Name = "Bearer",
+                                In = ParameterLocation.Header,
+
+                        },
+                        new List<string> ()
+                    }
+                });
+
+                c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseSwaggerConfig(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("v1/swagger.json", "v1");
+            });
+
+            return app;
+        }
+    }
+}
